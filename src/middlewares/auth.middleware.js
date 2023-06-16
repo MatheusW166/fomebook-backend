c965dcf39039d0a7e72431f921aa9b2a7872cdc0
MatheusW166 <matheuswagnerdossantos@outlook.com>
2023-06-16 14:03:26 -0300

    ref: searchs session decoded from jwt token

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
+import sessionRepository from "../repositories/session.repository.js";
 
-function validateToken(req, res, next) {
+async function validateToken(req, res, next) {
   const { authorization } = req.headers;
   const token = authorization?.replace("Bearer ", "");
 
@@ -13,11 +14,22 @@ function validateToken(req, res, next) {
   );
 
   if (decoded === null) {
-    return res.status(401).send({ error: "invalid token" });
+    return res.status(401).send({ error: "token expired or invalid" });
   }
 
-  req.session = decoded;
-  next();
+  try {
+    const sessionFound = await sessionRepository.findUniqueOrThrow({
+      where: { id: decoded.session },
+    });
+
+    req.session = sessionFound;
+    next();
+  } catch (err) {
+    if ((err.code = "P2025")) {
+      return res.status(401).send({ error: "session not found" });
+    }
+    return res.status(500).send({ error: "internal server error" });
+  }
 }
 
 const authMiddleware = { validateToken };
